e9a64f1a893070f289ca364e0c24d5ab6b988d2d
Arthur Ruf Hosang da Costa <arthur.rhc@gmail.com>
2020-04-30 21:53:27 -0300

    First commit

---
+++ citygen/install-macos.sh
@@ -0,0 +1,6 @@
+#conda activate project_ttc
+pb_tool compile
+cp -R ../citygen/ /System/Volumes/Data/Users/arthurrufhosangdacosta/Library/Application\ Support/QGIS/QGIS3/profiles/default/python/plugins/citygen/
+cd ..
+echo
+echo Done!
