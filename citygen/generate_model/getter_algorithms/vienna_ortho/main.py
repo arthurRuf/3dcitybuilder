b86404b9724dfc2b65d41751aead5537b30ac592
Arthur Ruf Hosang da Costa <arthur.rhc@gmail.com>
2020-05-02 10:35:03 -0300

    Implemented Vienna Ortho getter

--- citygen/generate_model/getter_algorithms/vienna_ortho/main.py
+++ citygen/generate_model/getter_algorithms/vienna_ortho/main.py
@@ -1,5 +1,5 @@
-import time, shutil, logging, os, sys, requests
-from ...bibliotecas import progress_bar, inputa, internet,plugin_management
+import time, shutil, os, sys, requests
+from qgis.core import QgsRasterLayer
 
 
 def configure(appResources, appContext):
@@ -7,8 +7,11 @@ def configure(appResources, appContext):
 
 
 def execute(appResources, appContext):
+    # https://maps.wien.gv.at/wmts/1.0.0/WMTSCapabilities-arcmap.xml
+    urlWithParams = 'crs=EPSG:3857&dpiMode=7&format=image/jpeg&layers=lb&styles=farbe&tileMatrixSet=google3857&url=https://maps.wien.gv.at/wmts/1.0.0/WMTSCapabilities-arcmap.xml'
 
-   # WMTS!!!
-   # https://maps.wien.gv.at/wmts/1.0.0/WMTSCapabilities-arcmap.xml
+    appContext.steps.gis.ortho.provider = "wms"
+    appContext.steps.gis.ortho.input_file = urlWithParams
 
-    logging.info("Done!")
+    appResources.bibliotecas.logger.update_progress(step_current=1, step_maximum=1)
+    appResources.bibliotecas.logger.plugin_log("Done!")
