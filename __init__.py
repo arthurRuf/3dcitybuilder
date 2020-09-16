b3f519f84b067ea4f93a78b2b72e81d79f8518a2
Arthur Ruf Hosang da Costa <arthur.rhc@gmail.com>
2020-09-15 23:25:05 -0300

    Removed mistaken folder Support

--- __init__.py
+++ __init__.py
@@ -32,5 +32,5 @@ def classFactory(iface):  # pylint: disable=invalid-name
     :type iface: QgsInterface
     """
     #
-    from .citygen import citygen
+    from .citygen_loader import citygen
     return citygen(iface)
