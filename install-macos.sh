e4602e544add5fa232cc4b28a171a18fb8aa0d71
Arthur Ruf Hosang da Costa <arthur.rhc@gmail.com>
2020-09-18 17:13:31 -0300

    Removed .sh and .bat scritps that were used to install the plugin during the development process
    * To perform this task, Makefile will be used from now on

--- install-macos.sh
+++
@@ -1,9 +0,0 @@
-
-rm -rf /System/Volumes/Data/Users/arthurrufhosangdacosta/Library/Application\ Support/QGIS/QGIS3/profiles/default/python/plugins/citygen
-cp -R . /System/Volumes/Data/Users/arthurrufhosangdacosta/Library/Application\ Support/QGIS/QGIS3/profiles/default/python/plugins/citygen/
-echo
-echo Done!
-
-
-# export PYTHONPATH="$PYTHONPATH:/Applications/QGIS3.12.app/Contents/MacOS/bin/python3"
-
